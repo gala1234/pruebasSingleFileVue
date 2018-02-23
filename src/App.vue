bcefa270107c40cbdca7e4f846364b609ed35751
gala1234 <calero.vg@gmail.com>
2018-02-23 10:30:27 +0100

    simple example of single file components

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+		<input class="input" type="search" placeholder="busqueda" v-model="message"></input>
+		<Input v-bind:msg="message"/>
+		<pre>{{ message }}</pre>
+  </div>
+</template>
+
+<script>
+import Input from './components/Input'
+
+export default {
+  name: 'App',
+  components: {
+		Input
+  },
+	data() {
+		return {
+			message: ''
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.input {
+	border: 1px solid black;
+}
+</style>
