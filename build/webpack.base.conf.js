64c2e2b0748ea3fd0603ac1f1a6bc66264caf44c
gala1234 <calero.vg@gmail.com>
2018-02-23 14:42:29 +0100

    attempt to import font

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -64,7 +64,15 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      }
+      },
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: utils.assetsPath('..static/fonts/[name].[hash:7].[ext]')
+				}
+			}
     ]
   },
   node: {
--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <img src="./assets/logo.png">
 		<input class="input" type="search" placeholder="busqueda" v-model="message"></input>
 		<Input v-bind:msg="message"/>
-		<pre>{{ message }}</pre>
+		<!-- <pre>{{ message }}</pre> -->
   </div>
 </template>
 
@@ -23,16 +23,5 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.input {
-	border: 1px solid black;
-}
-</style>
+<style src="./css/style.css"></style>
+<!-- <style src="./css/fonts.css"></style> AL PONERLO DA ERROR-->
